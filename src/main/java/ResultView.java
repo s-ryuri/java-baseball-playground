280365a8e1375d79e735458ecac1dfeb6b624698
s-ryuri <fbfbf1@naver.com>
2022-11-16 00:14:47 +0900

    Refactor : Code Format 변경

--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -20,31 +20,31 @@ public class ResultView {
             return;
         }
 
-        showBallAndStrike(ballCount,strikeCount);
+        showBallAndStrike(ballCount, strikeCount);
 
     }
 
-    private void showBallAndStrike(int ball,int strike){
+    private void showBallAndStrike(int ball, int strike) {
         System.out.println(ball + "볼 " + strike + "스트라이크");
 
     }
 
-    private void fourBall(){
+    private void fourBall() {
         System.out.println("Four ball or Nothing");
     }
-    public void choose(){
+    public void choose() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
     }
 
-    public void inputNumbers(){
+    public void inputNumbers() {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
-    public void correct(){
+    public void correct() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public void threeNumbers(){
+    public void threeNumbers() {
         System.out.println("3 자리수를 입력해주세요");
     }
 }
